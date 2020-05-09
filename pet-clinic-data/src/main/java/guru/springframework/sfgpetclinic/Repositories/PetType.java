4f10021ecfd0895feefdbc0e9ceb42e778a3243f
Chris Jurrens <crjurrens@gmail.com>
2020-05-08 19:06:47 -0600

    Added repositories. Closes #39.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/Repositories/PetType.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.Repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetType extends CrudRepository <PetType, Long>{
+}
